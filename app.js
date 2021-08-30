4020024bdca87cfa9930fa50af42dbf1f026ef73
kaishen lim <kaishen142253@gmail.com>
2021-08-30 08:47:01 +0800

    changed port to 3000

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 // This serves static files from the specified directory
-app.use(express.static(__dirname + "/index.html"));
+app.use(express.static(__dirname));
 
-const server = app.listen(8010, () => {
+const server = app.listen(3000, () => {
 
   const host = server.address().address;
   const port = server.address().port;
